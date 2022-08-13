d2d4f9e02e5f835e1fb54a3eaa1c6dbbd4eb9b1f
YadveerKaur <yadveerkaur5@gmail.com>
2022-08-12 22:15:00 -0500

    cprfinal

--- manipulating.c
+++ manipulating.c
@@ -2,16 +2,17 @@
 #define BUFFER_SIZE 80
 #include "manipulating.h"
 
+/* Purpose: To compare two different strings */
 void manipulating(void) {
 	printf("*** Start of Comparing Strings Demo ***\n");
 	char compare1[BUFFER_SIZE];
 	char compare2[BUFFER_SIZE];
 	int result;
-	while (TRUE) {
-		printf("Type the 1st string to compare (q - to quit):\n");
+	while (TRUE) {          // continue loop till the condition is true
+		printf("Type the 1st string to compare (q - to quit):\n"); 
 		gets(compare1);
 		
-		if (strcmp(compare1, "q") == 0) break; 
+		if (strcmp(compare1, "q") == 0) break; // strcmp compares the strings and  if statement checks if the condition provided is true, then break the code here.
 			printf("Type the 2nd string to compare:\n");
 			gets(compare2);
 			
